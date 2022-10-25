9ff70f05eb012f5d7c031cbe2bfa1e700882cd3e
Matej Krizan <krizan.matej79@gmail.com>
2022-10-25 21:54:40 +0200

    and again

--- z2/script.js
+++ z2/script.js
@@ -1,23 +1,23 @@
 const barberShops = {
     "Bratislavský kraj": {
-        "Bratislava I": ["Rado", "Julka", "Paťo"],
-        "Bratislava IV": ["Mišo", "Aďa", "Klement"],
-        "Senec": ["Fero", "Andrej"],
+        "Bratislava I": ["Rado", "Julka", "Paťo", "Je mi to jedno"],
+        "Bratislava IV": ["Mišo", "Aďa", "Klement", "Je mi to jedno"],
+        "Senec": ["Fero", "Andrej", "Je mi to jedno"],
     },
     "Nitriansky kraj": {
-        "Nitra": ["Milan", "Žofka", "Ala"],
-        "Komárno": ["Atila", "Maťka"],
-        "Šaľa": ["Ambróz", "Dávid"],
+        "Nitra": ["Milan", "Žofka", "Ala", "Je mi to jedno"],
+        "Komárno": ["Atila", "Maťka", "Je mi to jedno"],
+        "Šaľa": ["Ambróz", "Dávid", "Je mi to jedno"],
     },
     "Banskobystrický kraj": {
-        "Banská Bystrica": ["Romča", "Sisa", "Matúš", "Samo"],
-        "Brezno": ["Majo", "Zoltán"],
-        "Žiar nad Hronom": ["Evka", "Rišo"]
+        "Banská Bystrica": ["Romča", "Sisa", "Matúš", "Samo", "Je mi to jedno"],
+        "Brezno": ["Majo", "Zoltán", "Je mi to jedno"],
+        "Žiar nad Hronom": ["Evka", "Rišo", "Je mi to jedno"]
     },
-    "Košiscký kraj": {
-        "Košice I": ["Adela", "Jano", "Ďuro"],
-        "Košice IV": ["Zuzka", "Miška"],
-        "Trebišov": ["Oľga", "Igor"]
+    "Košický kraj": {
+        "Košice I": ["Adela", "Jano", "Ďuro", "Je mi to jedno"],
+        "Košice IV": ["Zuzka", "Miška", "Je mi to jedno"],
+        "Trebišov": ["Oľga", "Igor", "Je mi to jedno"]
     }
 }
 
@@ -29,12 +29,13 @@ function validateFirstName() {
         firstName.style.border = "2px solid red"
         error.style.color = "red"
         error.innerHTML = "Pole Meno nesmie byť prázdne!"
-        return
+        return false
     }
 
     firstName.style.border = "2px solid green"
     error.style.color = "green"
     error.innerHTML = "<i class='errorOk'>OK</i>"
+    return true
 }
 
 function validateLastName() {
@@ -45,12 +46,13 @@ function validateLastName() {
         lastName.style.border = "2px solid red"
         error.style.color = "red"
         error.innerHTML = "Pole Priezvisko nesmie byť prázdne!"
-        return
+        return false
     }
 
     lastName.style.border = "2px solid green"
     error.style.color = "green"
     error.innerHTML = "<i class='errorOk'>OK</i>"
+    return true
 }
 
 function validateEmail() {
@@ -62,18 +64,19 @@ function validateEmail() {
         email.style.border = "2px solid red"
         error.style.color = "red"
         error.innerHTML = "Pole E-mail nesmie byť prázdne!"
-        return
+        return false
     }
     if (!email.value.match(regex)) {
         email.style.border = "2px solid red"
         error.style.color = "red"
         error.innerHTML = "Pole E-mail nesplňuje požadovaný formát!"
-        return
+        return false
     }
 
     email.style.border = "2px solid green"
     error.style.color = "green"
     error.innerHTML = "<i class='errorOk'>OK</i>"
+    return true
 }
 
 function validatePhoneNumber() {
@@ -85,12 +88,13 @@ function validatePhoneNumber() {
         phoneNumber.style.border = "2px solid red"
         error.style.color = "red"
         error.innerHTML = "Nesplnený formát +421*********"
-        return
+        return false
     }
 
     phoneNumber.style.border = "2px solid green"
     error.style.color = "green"
     error.innerHTML = "<i class='ok'>OK</i>"
+    return true
 }
 
 function calculateAndSetAge() {
@@ -130,26 +134,43 @@ function validateAge() {
         age.style.border = "2px solid red"
         ageError.style.color = "red"
         ageError.innerHTML = "Pole Vek nesmie byť prázdne!"
-        return
+        return false
     }
 
     if (Number(age.value) < 0) {
         age.style.border = "2px solid red"
         ageError.style.color = "red"
         ageError.innerHTML = "Vek musí byť kladné číslo!"
-        return
+        return false
     }
 
     if (Number(age.value) !== ageFromBirthDate) {
         age.style.border = "2px solid red"
         ageError.style.color = "red"
         ageError.innerHTML = "Vek nesedí s dátumom narodenia!"
-        return
+        return false
     }
 
     age.style.border = "2px solid green"
     ageError.style.color = "green"
     ageError.innerHTML = "<i class='ok'>OK</i>"
+    return true
+}
+
+function generateSpecialService() {
+    let specialServiceDiv = document.getElementById("specialServiceDiv")
+    let specialServiceLabel = document.getElementById("specialServiceLabel")
+    let sexMale = document.getElementById("sexMale")
+    let sexFemale = document.getElementById("sexFemale")
+
+    if (sexMale.checked) {
+        specialServiceDiv.style.display = "flex"
+        specialServiceLabel.innerHTML = "Prajete si aj extra úpravu brady? (+ 10 €)"
+    }
+    if (sexFemale.checked) {
+        specialServiceDiv.style.display = "flex"
+        specialServiceLabel.innerHTML = "Prajete si aj extra úpravu obočia? (+ 10 €)"
+    }
 }
 
 function validateAppointmentDate() {
@@ -160,12 +181,13 @@ function validateAppointmentDate() {
         appointmentDate.style.border = "2px solid red"
         appointmentDateError.style.color = "red"
         appointmentDateError.innerHTML = "Pole Dátum stretnutia nesmie byť prázdne!"
-        return
+        return false
     }
 
     appointmentDate.style.border = "2px solid green"
     appointmentDateError.style.color = "green"
     appointmentDateError.innerHTML = "<i class='ok'>OK</i>"
+    return true
 }
 
 function validateAppointmentTime() {
@@ -176,12 +198,13 @@ function validateAppointmentTime() {
         appointmentTime.style.border = "2px solid red"
         appointmentTimeError.style.color = "red"
         appointmentTimeError.innerHTML = "Pole Čas stretnutia nesmie byť prázdne!"
-        return
+        return false
     }
 
     appointmentTime.style.border = "2px solid green"
     appointmentTimeError.style.color = "green"
     appointmentTimeError.innerHTML = "<i class='ok'>OK</i>"
+    return true
 }
 
 window.onload = function () {
