74f4e7040a3dc672315232e6e65ef2d527e4c8f6
krizek79 <krizan.matej79@gmail.com>
2022-12-03 15:57:37 +0100

    modal with change image functionality

--- z4/gallery/script.js
+++ z4/gallery/script.js
@@ -24,18 +24,19 @@ function init () {
 
 function createGalleryElements (data) {
     imagesContainer.replaceChildren()
-    data.forEach(item => {
+    for (let index in data) {
+        index = Number(index)
         let imageDiv = document.createElement("div")
         imageDiv.className = "col-lg-3 col-md-4 col-sm-6 p-1"
-        imageDiv.onclick = () => openModal(item)
+        imageDiv.onclick = () => openModal(index)
         imagesContainer.appendChild(imageDiv)
 
         let image = document.createElement("img")
         image.className = "img-thumbnail rounded"
-        image.src = item.path
+        image.src = data[index].path
         image.alt = "Ooops"
         imageDiv.appendChild(image)
-    })
+    }
 }
 
 function filterImages () {
@@ -54,6 +55,122 @@ function filterImages () {
     createGalleryElements(data)
 }
 
-function openModal (item) {
-    console.log("P**OVIN* SKU**VE**A PO*EBA**")
+function openModal (index) {
+    let currentImageIndex = index
+    let modalBodyArr = []
+
+    let modal = document.createElement("div")
+    modal.setAttribute("id", "modal")
+    mainSection.appendChild(modal)
+
+    //  Body
+    for (let index in imagesData) {
+        let modalBodyElements = generateModalBody(index);
+        modalBodyArr.push(modalBodyElements)
+    }
+
+    //  Overlay
+    let overlay = document.createElement("div")
+    overlay.setAttribute("id", "overlay")
+    document.body.appendChild(overlay)
+    overlay.addEventListener("click", () => closeModal(modal, overlay))
+
+    //  Header
+    let modalHeader = document.createElement("div")
+    modalHeader.setAttribute("id", "modalHeader")
+    modal.appendChild(modalHeader)
+
+    let modalNavigation = document.createElement("div")
+    modalNavigation.setAttribute("id", "modalNavigation")
+    modalHeader.appendChild(modalNavigation)
+
+    let previousBtn = document.createElement("button")
+    previousBtn.className = "button"
+    previousBtn.innerHTML = "Predošlá"
+    previousBtn.onclick = () => {
+        changeImage(modalBodyArr, currentImageIndex, -1)
+        currentImageIndex = getCurrentImageIndex(currentImageIndex, -1)
+    }
+    modalNavigation.appendChild(previousBtn)
+
+    let nextBtn = document.createElement("button")
+    nextBtn.className = "button"
+    nextBtn.innerHTML = "Ďalšia"
+    nextBtn.onclick = () => {
+        changeImage(modalBodyArr, currentImageIndex, 1)
+        currentImageIndex = getCurrentImageIndex(currentImageIndex, 1)
+    }
+    modalNavigation.appendChild(nextBtn)
+
+    let closeBtn = document.createElement("button")
+    closeBtn.setAttribute("id", "closeBtn")
+    closeBtn.innerHTML = "&times"
+    closeBtn.onclick = () => closeModal(modal, overlay)
+    modalHeader.appendChild(closeBtn)
+
+    //  Body
+    modalBodyArr.forEach(element => modal.appendChild(element))
+    modalBodyArr[currentImageIndex].style.display = "block"
+
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function generateModalBody (currentImageIndex) {
+    let modalBody = document.createElement("div")
+    modalBody.setAttribute("id", "modalBody")
+    modalBody.style.display = "none"
+
+    let modalImageDiv = document.createElement("div")
+    modalImageDiv.setAttribute("id", "modalImageDiv")
+    modalBody.appendChild(modalImageDiv)
+
+    let modalImage = document.createElement("img")
+    modalImage.setAttribute("id", "modalImage")
+    modalImage.src = imagesData[currentImageIndex].path
+    modalImage.alt = "Ooops"
+    modalImageDiv.appendChild(modalImage)
+
+    let modalBodyInfo = document.createElement("div")
+    modalBody.appendChild(modalBodyInfo)
+
+    let modalTitle = document.createElement("h4")
+    modalTitle.setAttribute("id", "modalTitle")
+    modalTitle.innerHTML = imagesData[currentImageIndex].title
+    modalBodyInfo.appendChild(modalTitle)
+
+    let modalDescription = document.createElement("p")
+    modalDescription.setAttribute("id", "modalDescription")
+    modalDescription.innerHTML = imagesData[currentImageIndex].description
+    modalBodyInfo.appendChild(modalDescription)
+
+    return modalBody
+}
+
+function closeModal (modal, overlay) {
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+function changeImage (modalBodyArr, current, value) {
+    modalBodyArr[current].style.display = "none"
+    current += value
+    if (current < 0) {
+        current = imagesData.length - 1
+    }
+    if (current > (imagesData.length - 1)) {
+        current = 0
+    }
+    modalBodyArr[current].style.display = "block"
+}
+
+function getCurrentImageIndex (current, value) {
+    current += value
+    if (current < 0) {
+        current = imagesData.length - 1
+    }
+    if (current > (imagesData.length - 1)) {
+        current = 0
+    }
+    return current
 }
