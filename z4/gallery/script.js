bb3c9391ca7018cd5f15bb41e53c146008e62471
krizek79 <krizan.matej79@gmail.com>
2022-12-07 16:13:03 +0100

    scroll on top after clicking on image

--- z4/gallery/script.js
+++ z4/gallery/script.js
@@ -28,7 +28,11 @@ function createGalleryElements (data) {
         index = Number(index)
         let imageDiv = document.createElement("div")
         imageDiv.className = "col-lg-3 col-md-4 col-sm-6 p-1"
-        imageDiv.onclick = () => openModal(data, index)
+        imageDiv.onclick = () => {
+            openModal(data, index)
+            document.body.scrollTop = 0
+            document.documentElement.scrollTop = 0
+        }
         imagesContainer.appendChild(imageDiv)
 
         let image = document.createElement("img")
